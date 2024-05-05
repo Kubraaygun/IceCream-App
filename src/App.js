7d444a1468fffb83dab373a9f28ce31024a00e29
kubraaygun <kubraaygun@outlook.com.tr>
2024-05-05 23:10:45 +0300

    Topping duzenlendi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Form from "./components/Form";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Scoops from "./components/Scoops";
+import Toppings from "./components/Toppings";
 
 const App = () => {
   return (
     <div>
       <Scoops/>
+      <Toppings/>
       <Form />
+     
     </div>
   );
 };
---
+++ src/components/Toppings/index.jsx
@@ -0,0 +1,37 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const Toppings = () => {
+    const [data,setData]=useState([])
+    const [basket,setBasket]=useState([])
+    useEffect(()=>{
+        axios.get("http://localhost:4000/toppings")
+        .then(res=>{setData(res.data)})
+    },[])
+//urune tiklandiginda calisir
+   const handleChange=(isChecked,item)=>{
+isChecked //eklenecek mi
+ ? setBasket([...basket,item])//sepete ekle
+:setBasket(basket.filter(i => i.name !== item.name))//sepetten kaldir
+   }
+  return (
+    <div className="container">
+        <h1>Sos Çeşitleri</h1>
+        <p>Tanesi <span className="text-success">3</span>₺</p>
+        <h3>Soslar Ücreti <span data-testid="total">{basket.length*3}</span>₺</h3>
+
+        <div className="row gap-3 p-3">{data.map((item)=>(
+            <div className="top-card" style={{width:"150px"}}>
+              <label htmlFor={item.name} className="d-flex flex-column align-items-center gap-3">
+              <img height={100} src={item.imagePath} alt="sos-resim" />
+              <p className="text-nowrap text-center">{item.name}</p>
+              </label>
+              <input onChange={(e)=>handleChange(e.target.checked,item)} id={item.name} type="checkbox" />
+             
+            </div>
+        ))}</div>
+        </div>
+  )
+}
+
+export default Toppings
